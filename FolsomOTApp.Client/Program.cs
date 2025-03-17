f5f3befe227ec4524853f344f088936c0a921481
eodknight23 <eodknight23@gmail.com>
2025-03-17 16:22:51 -0600

    Add collapsing nav menu library and implementation

--- FolsomOTApp.Client/Program.cs
+++ FolsomOTApp.Client/Program.cs
@@ -1,11 +1,13 @@
-using FolsomOTApp.Client;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using FolsomOTApp.Client;
+using CollapsibleNavMenu.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+builder.Services.AddScoped<INavMenuProvider, JsonNavMenuProvider>();
 
 await builder.Build().RunAsync();
