e78cd8fda081db5c739812fd5d0cef7f4565ab87
eodknight23 <eodknight23@gmail.com>
2025-03-17 18:54:49 -0600

    new-feature branch, attempting to fix the stuff I broke

---
+++ FolsomOTApp.Client/Layout/MainLayout.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace WoodooW.Client.Shared
+{
+    public partial class MainLayout
+    {
+
+        public bool ColapsedState { get; set; } = false;
+        private int expandedWidth = 250;
+        private int colapsedWidth = 0;
+        private string sidebarStyle = "";
+        private string mainStyle = "";
+
+        public MainLayout()
+        {
+            sidebarStyle = $"width: {expandedWidth}px";
+            mainStyle = $"margin-left: {expandedWidth}px";
+        }
+
+        public void Toggle()
+        {
+            if (ColapsedState)
+            {
+                sidebarStyle = $"width: {expandedWidth}px";
+                mainStyle = $"margin-left: {expandedWidth}px";
+                ColapsedState = false;
+            }
+            else
+            {
+                sidebarStyle = $"width: {colapsedWidth}px";
+                mainStyle = $"margin-left: {colapsedWidth}px";
+                ColapsedState = true;
+            }
+        }
+    }
+}
---
+++ FolsomOTApp.Client/Layout/NavMenu.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace WoodooW.Client.Shared
+{
+    public partial class NavMenu
+    {
+
+    }
+}
